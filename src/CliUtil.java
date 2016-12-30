2e5704c5876c70a3be5933612065aaf529f04d93
garzon <610204679@qq.com>
2016-12-30 19:04:47 +0800

    prettify the err msg

--- src/CliUtil.java
+++ src/CliUtil.java
@@ -3,14 +3,37 @@
  */
 
 import java.lang.*;
+import java.util.*;
 
 import miniJava.*;
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class CliUtil {
+    public static Vector<Token> getFirst10Tokens(ParserRuleContext ctx) {
+        Vector<Token> tokens = new Vector<>();
+        Iterator itr = ctx.children.iterator();
+        while(itr.hasNext()) {
+            ParseTree o = (ParseTree)itr.next();
+            if(o instanceof TerminalNode) {
+                TerminalNode tnode = (TerminalNode)o;
+                Token symbol = tnode.getSymbol();
+                tokens.add(symbol);
+                if(tokens.size() >= 10) break;
+            }
+        }
+        return tokens;
+    }
+
     public static void err(ParserRuleContext ctx, String msg) {
         Token firstToken = ctx.getStart();
-        System.err.printf("[ERR] @%d:%d in '%s':\n\t%s\n", firstToken.getLine(), firstToken.getCharPositionInLine(),
-                                ctx.getText(), msg);
+        String sourceContext = "";
+        for(Token token: getFirst10Tokens(ctx)) {
+            sourceContext += token.getText() + " ";
+        }
+        sourceContext += "...";
+        System.err.printf("[ERR] Line %d, Char %d, '%s':\n\t%s\n", firstToken.getLine(), firstToken.getCharPositionInLine(),
+                sourceContext, msg);
     }
 }
