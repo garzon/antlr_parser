7eedd8b9e31c96cd6a0ca1eb8c8c81e99673a371
garzon <610204679@qq.com>
2016-12-30 11:04:48 +0800

    fix the bug of auto visualizer script

--- show_parsed_tree.bat
+++ show_parsed_tree.bat
@@ -1,5 +1,5 @@
 rm build/ -rf
-mkdir build/
+mkdir build
 cp src/MiniJava.g4 build/
 cd build/
 antlr ./MiniJava.g4 && javac *.java && grun MiniJava goal -gui ../exampleMiniJavaProgram.minijava && cd ../
